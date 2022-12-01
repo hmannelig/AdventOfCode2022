3511033239b3e341335641288d3f8a8c3ece5b7f
Pablo Berjon <2490772B@student.gla.ac.uk>
2022-12-01 17:27:12 +0000

    initial commit

---
+++ Day 1/calorie_counting.py
@@ -0,0 +1,27 @@
+def is_smaller(calories, n) -> bool:
+    return calories < n
+
+
+f = open("calorie_counting.txt").read().splitlines()
+
+max_calories = second_max = third_max = temp = 0
+
+for line in f:
+
+    if line == '':
+        if is_smaller(max_calories, temp):
+            third_max = second_max
+            second_max = max_calories
+            max_calories = temp
+        elif is_smaller(second_max, temp):
+            third_max = second_max
+            second_max = temp
+        elif is_smaller(third_max, temp):
+            third_max = temp
+        temp = 0
+    else:
+        temp += int(line)
+
+total = max_calories + second_max + third_max
+print(total, max_calories, second_max, third_max)
+
